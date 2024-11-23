b5902a4a78924c8e71e6397e5d5de7e19838724e
Boyan <BKKyovtorov21@codingburgas.bg>
2024-11-23 20:15:15 +0200

    Implement LandingPage

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
-    engine.loadFromModule("Finbank", "Main");
+    engine.loadFromModule("Finbank", "LandingPage");
 
     return app.exec();
 }
